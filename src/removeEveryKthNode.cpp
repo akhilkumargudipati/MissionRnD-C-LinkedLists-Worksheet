57a1f381b7dfea2d8a6b120b054b287695baada0
akhilkumargudipati <akhilkumar1996.gudipati@gmail.com>
2016-01-08 15:34:00 +0530

    Submit Commit

--- src/removeEveryKthNode.cpp
+++ src/removeEveryKthNode.cpp
@@ -12,12 +12,63 @@ NOTES:
 */
 
 #include <stdio.h>
-
+#include<malloc.h>
 struct node {
 	int num;
 	struct node *next;
 };
 
-struct node * removeEveryKthNode(struct node *head, int K) {
-	return NULL;
+struct node * removeEveryKthNode(struct node *head, int K) 
+{
+	int temp;
+	struct node*temp1 = head;
+	if (head == NULL||K<=0)
+		return NULL;
+
+
+	if (K == 1)
+	{  
+		struct node*temp, *ptr;
+		ptr = head;
+		while (ptr != NULL)
+		{
+			temp = ptr->next;
+			free(ptr);
+			ptr = temp;
+		}
+		return NULL;
+	}
+	else
+	{
+		int m = K - 1, n = 1;
+		struct node *curr = head, *t;
+		int count;
+
+		// The main loop that traverses through the whole list
+		while (curr)
+		{
+			// Skip M nodes
+			for (count = 1; count<m && curr != NULL; count++)
+				curr = curr->next;
+
+			// If we reached end of list, then return
+			if (curr == NULL)
+				return head;
+
+			// Start from next node and delete N nodes
+			t = curr->next;
+			for (count = 1; count <= n && t != NULL; count++)
+			{
+				struct node *temp = t;
+				t = t->next;
+				free(temp);
+			}
+			curr->next = t; // Link the previous list with remaining nodes
+
+			// Set current pointer for next iteration
+			curr = t;
+		}
+		return head;
+	}
+	
 }
